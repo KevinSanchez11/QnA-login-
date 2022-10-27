442e2b80ad418a1ba8d2237ecc70384fc92764ad
KevinSanchez11 <113789340+KevinSanchez11@users.noreply.github.com>
2022-10-27 20:50:31 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+import "./Styles.css";
+
+function App() {
+  const [questions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h1>Questions And Answers About Login</h1>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
